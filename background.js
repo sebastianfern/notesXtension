d8b4b2e4fbf1ef54368990e086f843a103f4574c
sebastianfern <sfernandez973@gmail.com>
2023-08-28 15:34:00 -0700

    first-draft(create title, enter text and download)

--- background.js
+++ background.js
@@ -1,36 +1,33 @@
-// background.js
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({
-        text: 'OFF'
-    });
-});
+// let injectedTabIds = new Set();
 
-chrome.action.onClicked.addListener(async (tab) => {
-    // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+// chrome.action.onClicked.addListener(async (tab) => {
+//     const tabId = tab.id;
+    
+//     // Toggle the badge and handle the action click
+//     const prevState = await chrome.action.getBadgeText({ tabId: tabId });
+//     const nextState = prevState === 'ON' ? 'OFF' : 'ON';
 
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-        tabId: tab.id,
-        text: nextState
-    });
+//     await chrome.action.setBadgeText({
+//         tabId: tabId,
+//         text: nextState
+//     });
 
-    if (nextState === 'ON') {
-        // execute script
-        await chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            files: ['content-scripts/content.js'],
-        });
-    }
-});
+//     if (nextState === 'ON') {
+//         if (!injectedTabIds.has(tabId)) {
+//             // Inject the content script only if it has not been injected into this tab yet
+//             await chrome.scripting.executeScript({
+//                 target: { tabId: tabId },
+//                 files: ['content-scripts/content.js']
+//             }).catch(err => console.error('Error injecting script:', err)); // Error handling
+            
+//             // Add the tab ID to the set of injected tabs
+//             injectedTabIds.add(tabId);
+//         }
+//     } else {
+//         // Unload or disable the content script if you have a mechanism to do so
+//         // Remove the tab ID from the set of injected tabs
+//         injectedTabIds.delete(tabId);
+//     }
+// });
 
-// Listen for messages from popup.js
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'googleSignInSuccess') {
-        // Handle the Google Sign-In success, use message.data to get the authentication token or user information
-        console.log('Google Sign-In Success:', message.data);
-        // Perform further actions in your extension based on the sign-in data
-    }
-});
+
--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,53 +1,127 @@
-// Function to handle the "Submit" button click event
-function handleSubmission() {
-    // Get the value entered in the input field
-    const inputField = document.querySelector('.input-field');
-    const inputValue = inputField.value;
-  
-    // Create a new h2 element
-    const resultHeading = document.createElement('h2');
-  
-    // Set the submitted value as the text content of the h2 element
-    resultHeading.textContent = inputValue;
+// (function () {
+//   if (window.myExtensionScriptInjected) return;
+//   window.myExtensionScriptInjected = true;
 
-    // Assign an ID for CSS styling
-    resultHeading.id = 'submitted-value';
-    // Get the div with class "start-screen"
-  const startScreenDiv = document.querySelector('.start-screen');
+  console.log("Document body exists:", Boolean(document.body));
 
-  // Get the input field
-  const inputFieldElement = document.querySelector('.input-field');
+  fetch(chrome.runtime.getURL('content-scripts/content.html'))
+    .then(response => response.text())
+    .then(html => {
+      div.innerHTML = html;
 
-  // Insert the h2 element before the input field
-  startScreenDiv.insertBefore(resultHeading, inputFieldElement);
-}
-  
-  // Check if the div with id 'dropbox' already exists before creating it
-  if (!document.getElementById('dropbox')) {
-    // Create a new div element
-    let div = document.createElement('div');
-  
-    // Assign any attributes/styles
-    div.id = 'dropbox';
-  
-    // Fetch the content.html file
-    fetch(chrome.runtime.getURL('content-scripts/content.html'))
-      .then(response => response.text())
-      .then(html => {
-        // Set the content of the div to the loaded HTML from content.html
-        div.innerHTML = html;
-  
-        // Add event listener to the "Submit" button
-        const submitButton = div.querySelector('.submit-button');
+      submitButton = div.querySelector('.submit-button');
+      console.log("Submit button element:", submitButton);
+
+      if (submitButton) {
         submitButton.addEventListener('click', handleSubmission);
-      })
-      .catch(error => {
-        console.error('Error loading content.html:', error);
-        // If there's an error loading the file, you can provide a fallback message or handle it accordingly.
-        div.textContent = 'Error loading content.';
-      });
-  
-    // Append the div to the document body or any other desired location in the DOM
-    document.body.appendChild(div);
+      } else {
+        console.error('Submit button not found');
+      }
+    })
+    .catch(error => {
+      console.error('Error loading content.html:', error);
+    });
+
+  let div = document.createElement('div');
+  div.id = 'dropbox';
+  console.log("Div element:", div);
+
+  document.body.appendChild(div);
+
+  let activeInput = 'first';
+  let currentInputField;
+  let submitButton;
+  let savedTitle;
+
+  // Function to handle the "Submit" button click event
+  function handleSubmission() {
+    console.log("Handle Submission called");
+    currentInputField = document.querySelector('.input-field');
+    console.log("Current input field element:", currentInputField);
+
+    if (!currentInputField || !currentInputField.value.trim()) {
+      console.error("Input field is empty or not defined");
+      return;
+    }
+
+    if (activeInput === 'first') {
+      // Get the value entered in the input field
+      const inputValue = currentInputField.value;
+      savedTitle = inputValue;  // Save the title for later use
+
+      // Create a new h2 element or get the existing one if it exists
+      let resultHeading = document.querySelector('#submitted-value');
+      if (!resultHeading) {
+        resultHeading = document.createElement('h2');
+        resultHeading.id = 'submitted-value';
+        // Insert the new h2 element before the previous input field
+        const startScreenDiv = document.querySelector('.start-screen');
+        startScreenDiv.insertBefore(resultHeading, currentInputField);
+
+        // Change the text and color of the submit button
+        submitButton.textContent = 'Download';
+        submitButton.style.backgroundColor = 'red';
+      }
+
+      // Set the submitted value as the text content of the h2 element
+      resultHeading.textContent = inputValue;
+
+      // Create a new large empty input field or update the existing one if it exists
+      let largeInputField = document.querySelector('#lg-input-field');
+      if (!largeInputField) {
+        largeInputField = document.createElement('input');
+        largeInputField.type = 'text';
+        largeInputField.placeholder = 'Enter your notes...';
+        largeInputField.classList.add('lg-input-field');
+
+        // Insert the large input field below the new h2 element
+        const startScreenDiv = document.querySelector('.start-screen');
+        startScreenDiv.insertBefore(largeInputField, currentInputField);
+
+        // Remove the original input field and submit button
+        currentInputField.remove();
+        // submitButton.remove();
+
+        // Store the reference to the new input field
+        currentInputField = largeInputField;
+      }
+
+      // Switch to the second input field for the next submission
+      activeInput = 'second';
+      submitButton.removeEventListener('click', handleSubmission);
+      submitButton.addEventListener('click', handleDownload);
+    } else if (activeInput === 'second') {
+      // Switch back to the first input field for the next submission
+      activeInput = 'first';
+      submitButton.removeEventListener('click', handleDownload);
+      submitButton.addEventListener('click', handleSubmission);
+    }
   }
-  
+
+  function handleDownload() {
+    console.log("Handle Download called");
+    let largeInputField = document.querySelector('.lg-input-field');
+    console.log("lg-input-field element:", largeInputField);
+
+    if (!largeInputField || !largeInputField.value.trim()) {
+      console.error("lg-input-field is empty or not defined");
+      return;
+    }
+
+    const notesValue = largeInputField.value;
+
+    const blob = new Blob([notesValue], { type: "text/plain;charset=utf-8" });
+    const url = window.URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.style.display = "none";
+    a.href = url;
+    a.download = savedTitle ? `${savedTitle}.txt` : "notesXtension.txt";  // Use savedTitle if available
+
+    document.body.appendChild(a);
+    a.click();
+
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+  };
+// })();
